ca911df3e0b9aea03235502a627626f644341cb1
AnuragPradhan722 <pradhananurag222@gmail.com>
2024-11-10 17:06:56 -0600

    Update GradeBook.java

--- gradebook/GradeBook.java
+++ gradebook/GradeBook.java
@@ -19,17 +19,43 @@ public class GradeBook {
         while(true) {
             System.out.println("\nPlease make a selection:\n");
             System.out.println("1) List Class Grades");
-            System.out.println("2) Exit");
+                        System.out.println("2) Update Student Grade");
+            System.out.println("3) Exit");
             System.out.print("\nPlease choose an option: ");
             String choice = input.nextLine();
             System.out.println();
             switch(choice) {
                 case "1": // TODO: write a loop that, for each student prints 
                           //       lname, fname: grade 
-                    continue;
+                    for (Student student : students) {
+                        System.out.println(student.getLastName() + ", " + student.getFirstName() + ": " + student.getGrade());
+                    }
+                    break;
+        
                 case "2":
+                     System.out.print("Enter the first name of the student to update: ");
+                    String firstName = input.nextLine();
+                    boolean found = false;
+
+                     for (Student student : students) {
+                        if (student.getFirstName().equalsIgnoreCase(firstName)) {
+                            System.out.print("Enter the new grade for " + student.getFirstName() + " " + student.getLastName() + ": ");
+                            double newGrade = Double.parseDouble(input.nextLine());
+                            student.setGrade(newGrade);
+                            System.out.println("Grade updated!");
+                            found = true;
+                            break;
+                        }
+                     }
+                    if (!found) {
+                        System.out.println("Student not found.");
+                    }
+                    break;
+                case "3":
                     System.out.println("Goodbye!");
                     return;
+                default:
+                    System.out.println("Invalid option. Please try again.");
 
                 // Challenge: write another option to allow updating grades for a selected student
             }
